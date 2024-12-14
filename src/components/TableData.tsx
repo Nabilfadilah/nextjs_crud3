777c888f1b15f6902a9aee8583544cd5ec9e07c5
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-14 20:40:43 +0700

    Menjalankan fungsi mapping data dan menjalankan fungsi edit and delete

--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -1,39 +1,43 @@
 import Link from "next/link";
 import React from "react";
+import {getData} from "@/lib/action";
+import {DeleteButton} from "./Delete";
+import {formatDate} from "@/lib/utils";
 
-const TableData = () => {
+const Employee = async ({query}: {query: string}) => {
+  const employees = await getData(query);
+  //const employees = await getEmployeelist(query);
   return (
-    <div>
-      <table className="table table-zebra">
-        <thead className="text-sm text-gray-700 uppercase bg-gray-50">
-          <tr>
-            <th className="py-3 px-6">No</th>
-            <th className="py-3 px-6">Name</th>
-            <th className="py-3 px-6">Email</th>
-            <th className="py-3 px-6">Phone Number</th>
-            <th className="py-3 px-6">Created At</th>
-            <th className="py-3 px-6 text-center">Actions</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr key="" className="bg-white border-b text-black">
-            <td className="py-3 px-6">1</td>
-            <td className="py-3 px-6">Nabila</td>
-            <td className="py-3 px-6">nabil@gmail.com</td>
-            <td className="py-3 px-6">03020</td>
-            <td className="py-3 px-6">Des 19</td>
+    <table className="table table-header-group">
+      <thead className="text-sm text-gray-700 uppercase bg-gray-50">
+        <tr>
+          <th className="py-3 px-6">No.</th>
+          <th className="py-3 px-6">Name</th>
+          <th className="py-3 px-6">Email</th>
+          <th className="py-3 px-6">Phone Number</th>
+          <th className="py-3 px-6">Created At</th>
+          <th className="py-3 px-6 text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map((rs, index) => (
+          <tr key={rs.id} className="bg-white border-b text-black">
+            <td className="py-3 px-6">{index + 1}.</td>
+            <td className="py-3 px-6">{rs.name}</td>
+            <td className="py-3 px-6">{rs.email}</td>
+            <td className="py-3 px-6">{rs.phone}</td>
+            <td className="py-3 px-6">{formatDate(rs.createdAt.toString())}</td>
             <td className="flex justify-center gap-1 py-3">
-              <Link href={`/employee/edit/`} className="btn btn-info">
+              <Link href={`/employee/edit/${rs.id}`} className="btn btn-info">
                 Edit
               </Link>
-              | Delete
+              <DeleteButton id={rs.id} />
             </td>
           </tr>
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
-export default TableData;
+export default Employee;
